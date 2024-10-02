f98e2acca87723ec01c430863f864fd6756e82c6
Cristobal Betancurt <151901780+CristoxHD@users.noreply.github.com>
2024-10-02 18:02:03 -0300

    Update user.service.js
    Update de user.service.js para tarea N°3

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -41,3 +41,68 @@ export async function getUserService(id) {
         console.error("Error al obtener el usuario:", error);
     }
 }
+
+export async function getAllUserService(id) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userFound = await userRepository.find({
+            where: { id }
+        });
+
+        if (!userFound) {
+            return null;
+        }
+
+        userFound.createdAt = formatToLocalTime(userFound.createdAt);
+        userFound.updatedAt = formatToLocalTime(userFound.updatedAt);
+
+        return userFound;
+    } catch (error) {
+        console.error("Error al obtener todos los usuarios:", error);
+    }
+}
+
+export async function updateUserService(id, dataUser) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userUpdate = await userRepository.findOne({
+            where: { id }
+        });
+
+        if (!userUpdate) {
+            return null;
+        }
+
+        userRepository.merge(userFound, dataUser);
+
+        const userUpdated = await userRepository.save(userFound);
+
+        return userUpdated;
+    } catch (error) {
+        console.error("Error al actualizar el usuario: ", error);
+    }
+}
+
+
+
+export async function deleteUserService(id) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userDelete = await userRepository.findOne({ where:
+            { id }
+        });
+
+        if (!userDelete) {
+            return null;
+        }
+
+        const userDeleted = await userRepository.remove(userFound);
+
+        return userDeleted;
+    } catch (error) {
+        console.error("Error al eliminar el usuario: ", error);
+    }
+}
