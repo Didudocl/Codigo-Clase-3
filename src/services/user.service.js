bd9eb5baa517f065e2eae53522c08c839dd705cc
Deinielmm <daniel.monsalvez2101@alumnos.ubiobio.cl>
2024-10-02 00:25:05 -0300

    Update user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -41,3 +41,47 @@ export async function getUserService(id) {
         console.error("Error al obtener el usuario:", error);
     }
 }
+
+export async function getUsersService() {
+    const userRepository = AppDataSource.getRepository(User);
+
+        const users = await userRepository.findAll();
+
+        if(!users || users.length === 0) {
+            return res.status(404).json({
+                message: "No se encontraron usuarios",
+                data: null
+            })
+        }
+    return users;
+}
+
+export async function updateUserService(id, updatedData) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+        const userFound = await userRepository.findOne({ where: { id } });
+        if (!userFound){
+            return null;
+        }
+        await  userRepository.update(id,updatedData);
+        const updatedUser = await userRepository.findOne({ where: {id} });
+        return updatedUser;
+    } catch (error) {
+        console.error("Error al actualizar usuario: ",error);
+    }
+}
+
+export async function deleteUserService(id) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userFound = await userRepository.findOne({ where: { id} });
+        if (!userFound){
+            return null;
+        }
+        const userDeleted = await userRepository.remove(userFound);
+        return userDeleted;
+    } catch (error) {
+        console.error("Error al eliminar usuario:",error);
+    }
+}
