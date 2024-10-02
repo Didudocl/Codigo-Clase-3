975f5658692761c21e6978bba1b170c2fadc25ac
Bairon Sanhueza <bairon.sanhueza2001@alumnos.biobio.cl>
2024-10-02 00:47:41 -0400

    Modificacion de controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@
 import User from '../entity/user.entity.js';
 import { AppDataSource } from '../config/configDb.js';
 import { userBodyValidation } from '../validations/user.validation.js';
-import { createUserService, getUserService } from '../services/user.service.js';
+import { createUserService, getUserService, getUsersService, updateUserService, deleteUserService } from '../services/user.service.js';
 
 
 export async function createUser(req, res) {
@@ -51,9 +51,8 @@ export async function getUser(req, res) {
 
 export async function getUsers(req, res) {
     try {
-        const userRepository = AppDataSource.getRepository(User);
-
-        const users = await userRepository.find();
+    
+        const users = await getUsersService();
 
         if(!users || users.length === 0) {
             return res.status(404).json({
@@ -73,14 +72,17 @@ export async function getUsers(req, res) {
 
 export async function updateUser(req, res) {
     try {
-        const userRepository = AppDataSource.getRepository(User);
 
         const id = req.params.id;
         const user = req.body;
 
-        const userFound = await userRepository.findOne({
-            where: {id}
-        });
+        const { value, error } = userBodyValidation.validate(user);
+
+        if(error) return res.status(400).json({
+            message: error.message
+        })
+
+        const userFound = await getUserService(id);
 
         if(!userFound) {
             return res.status(404).json({
@@ -89,13 +91,9 @@ export async function updateUser(req, res) {
             });
         }
 
-        await userRepository.update(id, user);
+        await updateUserService(id, value);
 
-        const userData = await userRepository.findOne({
-            where: [{
-                id: id
-            }]
-        });
+        const userData = await getUserService(id);
 
         res.status(200).json({
             message: "Usuario actualizado correctamente",
@@ -109,13 +107,9 @@ export async function updateUser(req, res) {
 
 export async function deleteUser(req, res) {
     try {
-        const userRepository = AppDataSource.getRepository(User);
-
         const id = req.params.id;
 
-        const userFound = await userRepository.findOne({
-            where: {id}
-        });
+        const userFound = await getUserService(id);
 
         if(!userFound) {
             return res.status(404).json({
@@ -124,7 +118,7 @@ export async function deleteUser(req, res) {
             });
         }
 
-        const userDeleted = await userRepository.remove(userFound);
+        const userDeleted = await deleteUserService(userFound);
 
         res.status(200).json({
             message: "Usuario eliminado correctamente",
