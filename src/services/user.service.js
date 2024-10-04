f93f63caea3ce5644e95ff14aa1132bf812942c0
Danilo Fabiani Ceroni Rodriguez <danilo.ceroni1901@alumnos.ubiobio.cl>
2024-10-04 15:27:11 -0300

    Update user.service.js

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -18,6 +18,7 @@ export async function createUserService(dataUser) {
         return userSaved;
     } catch (error) {
         console.error('Error al crear un usuario: ', error);
+        throw error;
     }
 }
 
@@ -39,5 +40,62 @@ export async function getUserService(id) {
         return userFound;
     } catch (error) {
         console.error("Error al obtener el usuario:", error);
+        throw error;
+    }
+}
+
+export async function getUsersService() {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+        const users = await userRepository.find();
+        return users;
+    } catch (error) {
+        console.error('Error al obtener usuarios:', error);
+        throw error;
+    }
+}
+
+export async function updateUserService(id, userData) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+        
+        const userFound = await userRepository.findOne({
+            where: { id }
+        });
+
+        if (!userFound) {
+            return null;
+        }
+
+        await userRepository.update(id, userData);
+
+        const updatedUser = await userRepository.findOne({
+            where: { id }
+        });
+
+        return updatedUser;
+    } catch (error) {
+        console.error("Error al actualizar el usuario:", error);
+        throw error;
+    }
+}
+
+export async function deleteUserService(id) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+        
+        const userFound = await userRepository.findOne({
+            where: { id }
+        });
+
+        if (!userFound) {
+            return null;
+        }
+
+        const userDeleted = await userRepository.remove(userFound);
+        return userDeleted;
+    } catch (error) {
+        console.error("Error al eliminar el usuario:", error);
+        throw error;
     }
 }
