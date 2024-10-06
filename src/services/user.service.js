0b2eec8bf19a544b2f0aa316693c73cae0719bfc
EmilioAlarconGomez <emilio.alarcon.gomez@gmail.com>
2024-10-06 06:18:45 -0300

    ADD Services

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -38,6 +38,67 @@ export async function getUserService(id) {
 
         return userFound;
     } catch (error) {
-        console.error("Error al obtener el usuario:", error);
+        console.error("Error al obtener el usuario:", error); 
+    }
+}
+
+export async function getUsersService() {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const usersFound = await userRepository.find();
+
+        usersFound.forEach(user => {
+            user.createdAt = formatToLocalTime(user.createdAt);
+            user.updatedAt = formatToLocalTime(user.updatedAt);
+        });
+
+        return usersFound;
+    } catch (error) {
+        console.error("Error al obtener los usuarios:", error);
+    }
+}
+
+export async function updateUserService(id, dataUser) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userFound = await userRepository.findOne({
+            where: { id }
+        });
+
+        if (!userFound) {
+            return null;
+        }
+
+        userFound.nombreCompleto = dataUser.nombreCompleto;
+        userFound.rut = dataUser.rut;
+        userFound.email = dataUser.email;
+
+        const userUpdated = await userRepository.save(userFound);
+
+        return userUpdated;
+    } catch (error) {
+        console.error("Error al actualizar el usuario:", error);
+    }
+}
+
+export async function deleteUserService(id) {
+    try {
+        const userRepository = AppDataSource.getRepository(User);
+
+        const userFound = await userRepository.findOne({
+            where: { id }
+        });
+
+        if (!userFound) {
+            return null;
+        }
+
+        await userRepository.remove(userFound);
+
+        return userFound;
+    } catch (error) {
+        console.error("Error al eliminar el usuario:", error);
     }
 }
